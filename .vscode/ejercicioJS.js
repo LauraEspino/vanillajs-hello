7036992425d945660bc3062d741e694abeb5527b
LauraEspino <126553407+LauraEspino@users.noreply.github.com>
2023-04-19 19:33:23 +0000

    sin terminar día 1

--- .vscode/ejercicioJS.js
+++ .vscode/ejercicioJS.js
@@ -3,17 +3,31 @@ let action = ['ate','peed','crushed','broke'];
 let what = ['my homework', 'the keys', 'the car'];
 let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
 
-let onLoad();
-
-var random1(){
+function random_items1(items1){
+    var random1="";{
     return who[Math.floor(Math.random()*who.length)];
 }
-var random2()={
-    return action[Math.floor(Math.random()*action.length)];
+    console.log(random_items1(random1));
 }
-var random3()={
+function random_items2(items2){
+    var random2="";{
+    return action[Math.floor(Math.random()*action.length)];
+
+}
+    console.log(random_items2(random2));
+}
+function random_items3(items3){
+    var random3="";{
     return what[Math.floor(Math.random()*what.length)];
 }
-var random4()={
+    console.log(random_items3(random3));
+}
+function random_items4(items4){
+    var random4="";{
     return when[Math.floor(Math.random()*when.length)];
+}
+    console.log(random_items3(random3));
+}
+let onLoad(randomParagraph){
+/* sumar console.log 
 }
