38702c64102112792a94d10d64a568b4c8f3ab81
LauraEspino <126553407+LauraEspino@users.noreply.github.com>
2023-04-22 16:57:40 +0000

    impresión excusa

--- .vscode/ejercicioJS.js
+++ .vscode/ejercicioJS.js
@@ -1,46 +1,20 @@
-let who= ['The dog','My grandma','His turtle','My bird'];
-let aleatorioWho= Math.floor(Math.random()*who.length);
+var who= ['The dog','My grandma','His turtle','My bird'];
+var randomWho= who[Math.floor(Math.random()*who.length)];
+var excuse1= document.write(randomWho);
+
+var action = ['ate','peed','crushed','broke'];
+var randomAction= action[Math.floor(Math.random()*action.length)];
+var excuse2= document.write(randomAction);
+
+var what = ['my homework', 'the keys', 'the car'];
+var randomWhat= what[Math.floor(Math.random()*what.length)];
+var excuse3= document.write(randomWhat);
+
+var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+var randomWhen= when[Math.floor(Math.random()*when.length)];
+var excuse4= document.write(randomWhen);
+
+var espacio= " ";
+var onload=(excuse1+espacio+excuse2+espacio+excuse3+espacio+excuse4);
 
 
-let action = ['ate','peed','crushed','broke'];
-let aleatorioAction= Math.floor(Math.random()*action.length);
-
-let what = ['my homework', 'the keys', 'the car'];
-let aleatorioWhat= Math.floor(Math.random()*what.length);
-
-let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-let aleatorioWhen= Math.floor(Math.random()*when.length);
-
-let onload=aleatorioWho+" "+aleatorioAction+" "+aleatorioWhat+" "+aleatorioWhen;
-console.log(onload);
-/*
-function random_items1(items1){
-    var random1="";{
-    return who[Math.floor(Math.random()*who.length)];
-}
-    console.log(random_items1(random1));
-}
-function random_items2(items2){
-    var random2="";{
-    return action[Math.floor(Math.random()*action.length)];
-
-}
-    console.log(random_items2(random2));
-}
-function random_items3(items3){
-    var random3="";{
-    return what[Math.floor(Math.random()*what.length)];
-}
-    console.log(random_items3(random3));
-}
-function random_items4(items4){
-    var random4="";{
-    return when[Math.floor(Math.random()*when.length)];
-}
-    console.log(random_items3(random3));
-}
-let onLoad(randomParagraph){
-    var randomParagraph= random_items1+random_items2+random_items3+random_items4;
-    console.log(randomParagraph);
-}
-console.log(onload(randomParagraph));
