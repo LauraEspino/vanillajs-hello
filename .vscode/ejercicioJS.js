02e77db85101132b895bf888122bc079f7983999
LauraEspino <126553407+LauraEspino@users.noreply.github.com>
2023-04-23 14:24:13 +0000

    fin proyecto JS-1

--- .vscode/ejercicioJS.js
+++ .vscode/ejercicioJS.js
@@ -1,20 +1,20 @@
-function oracion(){
-var who= ['The dog ','My grandma ','His turtle ','My bird '];
-var randomWho= who[Math.floor(Math.random()*who.length)];
-var excuse1= document.write(randomWho);
+function onload(){
+        var who= ['The dog ','My grandma ','His turtle ','My bird '];
+        var randomWho= who[Math.floor(Math.random()*who.length)];
+        var excuse1= document.write(randomWho);
 
-var action = ['ate ','peed ','crushed ','broke '];
-var randomAction= action[Math.floor(Math.random()*action.length)];
-var excuse2= document.write(randomAction);
+        var action = ['ate ','peed ','crushed ','broke '];
+        var randomAction= action[Math.floor(Math.random()*action.length)];
+        var excuse2= document.write(randomAction);
 
-var what = ['my homework ', 'the keys ', 'the car '];
-var randomWhat= what[Math.floor(Math.random()*what.length)];
-var excuse3= document.write(randomWhat);
+        var what = ['my homework ', 'the keys ', 'the car '];
+        var randomWhat= what[Math.floor(Math.random()*what.length)];
+        var excuse3= document.write(randomWhat);
 
-var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
-var randomWhen= when[Math.floor(Math.random()*when.length)];
-var excuse4= document.write(randomWhen);
-}
+        var when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+        var randomWhen= when[Math.floor(Math.random()*when.length)];
+        var excuse4= document.write(randomWhen);
+    }
 
 
 
